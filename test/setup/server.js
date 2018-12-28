d43889e0de13db87d0d9330040a21ed23c7486c0
Haim Rubin <haim.rubin@gmail.com>
2018-12-28 20:28:30 +0200

    Remove unused resource from test

--- test/setup/server.js
+++ test/setup/server.js
@@ -1,12 +1,8 @@
 import { server } from '../../index'
-import configOption from '../../src/configOption.json' 
 import config from './app.dev.config'
 
 export default () => (
-  server({
-    //...configOption,
-    ...config,
-  })
+  server(config)
     .then(({ host, port, stop }) => {
       console.log(`Server run on: ${host}${port? ':' + port: ''}`)
       return { host, port, stop }
