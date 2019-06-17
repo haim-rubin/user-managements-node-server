95c5085b0e71e88df75703caaed78abec5b8db9c
Haim Rubin <haim.rubin@gmail.com>
2019-06-17 22:09:27 +0300

    Updated config + added db files

---
+++ test/sign-up/helper.js
@@ -0,0 +1,79 @@
+import {
+  credentials,
+  baseUrl,
+  signUpRoute,
+  signInRoute
+} from '../data'
+import { chaiRequest } from '../setup/chaiHttpHelper'
+import config from '../setup/app.dev.config.json'
+import { logger } from '../setup/mocks/logger'
+import initEntities from '../../src/entities'
+import path from 'path'
+import createServer from '../setup'
+import create from '../../scripts/create-database'
+import util from 'util'
+import fs from 'fs'
+import uuid from 'uuid'
+export const dbWithUserDoesNotActivated = path.join( __dirname, '../data/db-with-user-does-not-activated.sqlite')
+export const dbWithActivatedUser = path.join( __dirname, '../data/db-with-activated-user.sqlite')
+const copyFile = util.promisify(fs.copyFile)
+import { server } from '../../index'
+
+const { ActionVerifications } = initEntities({ config: config.database, logger })
+const request = chaiRequest(baseUrl)
+
+export const getDbConfigWithInactivrUser = () => {
+    const dbTest = `/tmp/${uuid.v4()}${path.extname(dbWithUserDoesNotActivated)}`
+    const database = {
+        ...config.database,
+        settings: {
+            ...config.database.settings,
+            storage: dbTest
+        }
+    }
+    return (
+        copyFile(
+            dbWithUserDoesNotActivated,
+            dbTest
+        )
+        .then(() => ({ database, dbTest }))
+        .then(({ database, dbTest }) => (
+            initEntities({ config: database, logger })
+        ))
+        .then(entities =>{
+            server({ ...config, database })
+        })
+    )
+}
+export const initServerWithUserInactiveUser = (config) => {
+    return (
+        create({ config })
+            .then(createServer)
+    )
+}
+
+export const tryCreateUser = () => (
+    request
+        .post(signUpRoute)
+        .send(credentials)
+)
+
+export const tryLogin = () => (
+    request
+        .post(signInRoute)
+        .send(credentials)
+)
+
+export const createUserAndActivate = () => (
+    tryCreateUser()
+        .end(async err => {
+            const { actionId } = await ActionVerifications
+                .findOne({ where: { username: credentials.username }})
+                .then(extract)
+
+            return (
+                request
+                    .get(`${verifyRoute}/${actionId}`)
+            )
+        })
+)
